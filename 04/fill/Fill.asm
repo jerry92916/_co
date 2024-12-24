8d10e3f872f0f0e193c12001ec3d279299147176
jerry92916 <166776207+jerry92916@users.noreply.github.com>
2024-12-24 15:05:44 +0800

    Update Fill.asm

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -11,4 +11,40 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(LOOP)
+  @KBD
+  D=M
+  @FILL
+  D;JGT
+
+(CLEAR)
+  @offset
+  D=M
+  @SCREEN
+  A=D+A
+  M=0
+
+  @INC_OFFSET
+  0;JMP
+
+(FILL)
+  @offset
+  D=M
+  @SCREEN
+  A=D+A
+  M=-1
+
+(INC_OFFSET)
+  @offset
+  MD=M+1
+  @8192
+  D=D-A
+
+  @LOOP
+  D;JNE
+
+(RESET_OFFSET)
+  @offset
+  M=0
+  @LOOP
+  0;JMP
